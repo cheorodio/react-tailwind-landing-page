fc1715e3e70f070a8f667630343388c1bcaf9d6e
cheorodio <cheorodio@gmail.com>
2023-07-24 13:09:14 +0200

    Add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
 export default function Navbar() {
-  // const links = {};
+  const links = [
+    { id: 1, title: 'About', link: '/' },
+    { id: 2, title: 'Service', link: '/' },
+    { id: 3, title: 'Projects', link: '/' },
+    // { id: 4, title: 'Contact', link: '/' },
+  ];
+
   return (
-    <nav>
-      <div>
-        <img
-          src="https://uploads-ssl.webflow.com/63fffb3513dd1b7acde9449c/63ffff448a159077d9fd75db_Logo.svg"
-          alt="logo"
-        />
-      </div>
+    <nav className="flex text-justify justify-between items-center py-10 px-11 absolute z-10 w-full">
+      <p className="font-barlow text-4xl font-bold text-white">sunnyside</p>
+      <ul className="flex text-justify justify-between items-center gap-12 text-lg text-white">
+        {links.map(({ id, title, link }) => (
+          <a href={link} key={`key-li-${id}`}>
+            <li>{title}</li>
+          </a>
+        ))}
+        <li>
+          <a className="text-black py-4 px-9 bg-white rounded-3xl uppercase font-fraunces text-sm">
+            Contact
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
