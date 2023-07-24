07c451b86abe21cf3e798f9b6591ef78c385c17a
cheorodio <cheorodio@gmail.com>
2023-07-24 14:25:53 +0200

    Add and style a section

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,6 +14,22 @@ function Home() {
           alt="arrow down"
         />
       </section>
+      <section className="flex">
+        <div className="flex flex-col justify-center pl-40 pr-24 gap-9">
+          <h2 className="font-fraunces text-[40px] tracking-wide leading-tight">
+            Transform your brand
+          </h2>
+          <p className="font-barlow text-[18px] text-darkGrayBlue">
+            We are a full-service creative agency specializing in helping brands
+            grow fast. Engage your clients through compelling visuals that do
+            most of the marketing for you.
+          </p>
+          <a className="uppercase font-fraunces text-base after:content-['']">
+            Learn more
+          </a>
+        </div>
+        <img src="/images/image-transform.jpg" alt="egg" />
+      </section>
     </main>
   );
 }
