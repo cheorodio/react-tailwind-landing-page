6232c4ab71164b63b5b8d663b83032127c619c4a
cheorodio <cheorodio@gmail.com>
2023-07-24 13:08:59 +0200

    Add colors and font family

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: 'hsl(7, 99%, 70%)',
+        yellow: 'hsl(51, 100%, 49%)',
+        cyan: 'hsl(167, 40%, 24%)',
+        blue: 'hsl(198, 62%, 26%)',
+        lightCyan: 'hsl(168, 34%, 41%)',
+      },
+      fontFamily: {
+        fraunces: ['Fraunces'],
+        barlow: ['Barlow'],
+      },
+    },
   },
   plugins: [],
 };
