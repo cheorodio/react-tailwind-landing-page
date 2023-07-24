18bf5dd8466ae09197900a660142ffcd8d17d3fc
cheorodio <cheorodio@gmail.com>
2023-07-24 16:14:38 +0200

    Add a section

--- src/components/Home.js
+++ src/components/Home.js
@@ -30,6 +30,22 @@ function Home() {
         </div>
         <img src="/images/image-transform.jpg" alt="egg" />
       </section>
+      <section className="flex">
+        <img src="/images/image-stand-out.jpg" alt="cup" />
+        <div className="flex flex-col justify-center pl-28 pr-40 gap-9">
+          <h2 className="font-fraunces text-[40px] tracking-wide leading-tight">
+            Stand out to the right audience
+          </h2>
+          <p className="font-barlow text-[18px] text-grayBlue">
+            Using a collaborative formula of designers, researchers,
+            photographers, videographers, and copywriters, we'll build and
+            extend your brand in digital places.
+          </p>
+          <a className="uppercase font-fraunces relative before:absolute before:top-3/4 before:left-0 before:h-2 before:w-32 before:-z-10 before:rounded-3xl w-32 text-center before:bg-lightRed">
+            Learn more
+          </a>
+        </div>
+      </section>
     </main>
   );
 }
