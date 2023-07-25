e0fb0e2550667f59ba8d18a9e3e211e72533bbd7
cheorodio <cheorodio@gmail.com>
2023-07-25 12:00:50 +0200

    Create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="h-100 bg-footerBg flex items-center justify-center flex-col gap-12 pt-16 pb-24">
+      <img src="images/logo.svg" alt="logo" className="w-44" />
+      <div className="flex flex-col items-center gap-24">
+        <ul className="flex gap-16 font-barlow text-lg text-lightCyan">
+          <li>
+            <a>About</a>
+          </li>
+          <li>
+            <a>Services</a>
+          </li>
+          <li>
+            <a>Projects</a>
+          </li>
+        </ul>
+        <div className="flex gap-7">
+          <img src="/images/icon-facebook.svg" alt="facebook icon" />
+          <img src="/images/icon-instagram.svg" alt="instagram icon" />
+          <img src="/images/icon-twitter.svg" alt="twitter icon" />
+          <img src="/images/icon-pinterest.svg" alt="pinterest icon" />
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
